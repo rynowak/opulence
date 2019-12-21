e26ea68f6421362e8177739fa54749a4a6d5baa2
Ryan Nowak <nowakra@gmail.com>
2019-12-21 15:28:54 -0800

    Add project skeleton

---
+++ src/dotnet-opulence/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Opulence
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
